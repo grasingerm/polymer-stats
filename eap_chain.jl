90ab5cbb4790d334a22747350401270b028c2421
Matthew Grasinger <grasingerm@gmail.com>
2020-09-22 16:31:14 -0400

    implemented callback functions for tracking the mcmc trajectory through phase space

---
+++ eap_chain.jl
@@ -0,0 +1,147 @@
+using Distributions;
+
+CnMatrix = AbstractMatrix{Int};
+FdMatrix = AbstractMatrix{Float64};
+CnVector = AbstractVector{Int};
+FdVector = AbstractVector{Float64};
+
+ϕ_dist = Uniform(0.0, 2*π);
+θ_dist = Uniform(0.0, π);
+
+mutable struct EAPChain
+  b::Real;
+  E0::Real;
+  K1::Real;
+  K2::Real;
+  kT::Real;
+  Fz::Real;
+  Fx::Real;
+  ϕs::FdVector;
+  cϕs::FdVector;
+  sϕs::FdVector;
+  θs::FdVector;
+  cθs::FdVector;
+  sθs::FdVector;
+  μs::FdMatrix;
+  us::FdVector;
+  xs::FdMatrix;
+  r::FdVector;
+  U::Real;
+end
+
+@inline n(chain::EAPChain) = length(chain.ϕs);
+
+@inline n̂(cϕ, sϕ, cθ, sθ) = [cϕ * sθ; sϕ * sθ; cθ];
+
+function μ(E0::Real, K1::Real, K2::Real, cϕ::Real, sϕ::Real, cθ::Real, sθ::Real)
+  n̂i = n̂(cϕ, sϕ, cθ, sθ);
+  return ((K1 - K2) * E0 * cθ * n̂i) + (K2 * [0.0; 0.0; E0]);
+end
+
+@inline n̂j(chain::EAPChain, idx::Int) = n̂(chain.cϕs[idx], chain.sϕs[idx],
+                                          chain.cθs[idx], chain.sθs[idx]);
+
+function update_xs!(chain::EAPChain, idx::Int=1)
+  if idx == 1
+    chain.xs[:, idx] = chain.b / 2 * n̂j(chain, idx);
+    idx += 1;
+  end
+  for i=idx:n(chain)
+    chain.xs[:, i] = (chain.xs[:, i-1] + 
+                      chain.b / 2 * (n̂j(chain, i) + n̂j(chain, i-1))); 
+  end
+end
+
+@inline u(E0::Real, μ::FdVector) = -1/2*E0*μ[3];
+
+function EAPChain(pargs::Dict)
+  ϕs = rand(ϕ_dist, pargs["num-monomers"]);
+  θs = rand(θ_dist, pargs["num-monomers"]);
+
+  ret =  EAPChain(
+                  pargs["mlen"],
+                  pargs["E0"],
+                  pargs["K1"],
+                  pargs["K2"],
+                  pargs["kT"],
+                  pargs["Fz"],
+                  pargs["Fx"],
+                  ϕs,
+                  map(cos, ϕs),
+                  map(sin, ϕs),
+                  θs,
+                  map(cos, θs),
+                  map(sin, θs),
+                  zeros(3, pargs["num-monomers"]),
+                  zeros(pargs["num-monomers"]),
+                  zeros(3, pargs["num-monomers"]),
+                  zeros(3),
+                  0.0
+                 );
+  for i=1:n(ret)
+    ret.μs[:, i] = μ(ret.E0, ret.K1, ret.K2, 
+                     ret.cϕs[i], ret.sϕs[i], ret.cθs[i], ret.sθs[i]);
+  end
+  ret.us[:] = map(i -> u(ret.E0, view(ret.μs, :, i)), 1:n(ret));
+  update_xs!(ret);
+  ret.r[:] = end_to_end(ret);
+  ret.U = U(ret);
+  return ret;
+end
+
+# TODO: consider rewriting this to make better use of past calculations
+# this is probably by far the slowest calculation *shrug emoji*
+function U_interaction(chain::EAPChain)
+  U = 0.0;
+  for i=1:n(chain), j=i+1:n(chain) # once debugged, use inbounds
+    r = chain.xs[:, i] - chain.xs[:, j];
+    r2 = dot(r, r);
+    rmag = sqrt(r2);
+    r̂ = r / rmag;
+    r3 = r2*rmag;
+    μi = view(chain.μs, :, i);
+    μj = view(chain.μs, :, i);
+    U += (dot(μi, μj) - 3*dot(μi, r̂)*dot(μj, r̂)) / (4*π);
+  end
+  return U;
+end
+
+function move!(chain::EAPChain, idx::Int, dϕ::Real, dθ::Real)
+  ϕprev = chain.ϕs[idx];
+  chain.ϕs[idx] += dϕ;
+  if chain.ϕs[idx] < 0
+    chain.ϕs[idx] += 2*π;
+  elseif chain.ϕs[idx] >= 2*π
+    chain.ϕs[idx] -= 2*π;
+  end
+  chain.cϕs[idx] = cos(chain.ϕs[idx]);
+  chain.sϕs[idx] = sin(chain.ϕs[idx]);
+
+  θprev = chain.θs[idx]; 
+  chain.θs[idx] += dθ;
+  if chain.θs[idx] < 0
+    chain.θs[idx] = 0;
+  elseif chain.θs[idx] >= π
+    chain.θs[idx] = π;
+  end
+  chain.cθs[idx] = cos(chain.θs[idx]);
+  chain.sθs[idx] = sin(chain.θs[idx]);
+
+  chain.μs[:, idx] = μ(chain.E0, chain.K1, chain.K2,
+                       chain.cϕs[idx], chain.sϕs[idx], 
+                       chain.cθs[idx], chain.sθs[idx]);
+  chain.us[idx] = u(chain.E0, view(chain.μs, :, idx));
+  update_xs!(chain, idx);
+  chain.r[:] = end_to_end(chain);
+  return (chain.ϕs[idx] - ϕprev, chain.θs[idx] - θprev);
+end
+
+@inline end_to_end(chain::EAPChain) = (chain.xs[:, end] + 
+                                       chain.b/2*n̂j(chain, n(chain)));
+
+@inline function U(chain::EAPChain)
+  return (sum(chain.us) + U_interaction(chain)
+          - dot(end_to_end(chain), [chain.Fx; 0.0; chain.Fz]));
+end
+
+@inline chain_μ(chain::EAPChain) = sum(map(i -> chain.μs[:, i], 1:n(chain)));
--- interacting_chain.jl
+++ interacting_chain.jl
@@ -3,144 +3,7 @@ using Distributions;
 using LinearAlgebra;
 using Logging;
 
-CnMatrix = AbstractMatrix{Int};
-FdMatrix = AbstractMatrix{Float64};
-CnVector = AbstractVector{Int};
-FdVector = AbstractVector{Float64};
-
-ϕ_dist = Uniform(0.0, 2*π);
-θ_dist = Uniform(0.0, π);
-
-struct EAPChain
-  b::Real;
-  E0::Real;
-  K1::Real;
-  K2::Real;
-  kT::Real;
-  Fz::Real;
-  Fx::Real;
-  ϕs::FdVector;
-  cϕs::FdVector;
-  sϕs::FdVector;
-  θs::FdVector;
-  cθs::FdVector;
-  sθs::FdVector;
-  μs::FdMatrix;
-  us::FdVector;
-  xs::FdMatrix;
-end
-
-@inline n(chain::EAPChain) = length(chain.ϕs);
-
-@inline n̂(cϕ, sϕ, cθ, sθ) = [cϕ * sθ; sϕ * sθ; cθ];
-
-function μ(E0::Real, K1::Real, K2::Real, cϕ::Real, sϕ::Real, cθ::Real, sθ::Real)
-  n̂i = n̂(cϕ, sϕ, cθ, sθ);
-  return ((K1 - K2) * E0 * cθ * n̂i) + (K2 * [0.0; 0.0; E0]);
-end
-
-@inline n̂j(chain::EAPChain, idx::Int) = n̂(chain.cϕs[idx], chain.sϕs[idx],
-                                          chain.cθs[idx], chain.sθs[idx]);
-
-function update_xs!(chain::EAPChain, idx::Int=1)
-  if idx == 1
-    chain.xs[:, idx] = chain.b / 2 * n̂j(chain, idx);
-    idx += 1;
-  end
-  for i=idx:n(chain)
-    chain.xs[:, i] = (chain.xs[:, i-1] + 
-                      chain.b / 2 * (n̂j(chain, i) + n̂j(chain, i-1))); 
-  end
-end
-
-@inline u(E0::Real, μ::FdVector) = -1/2*E0*μ[3];
-
-function EAPChain(pargs::Dict)
-  ϕs = rand(ϕ_dist, pargs["num-monomers"]);
-  θs = rand(θ_dist, pargs["num-monomers"]);
-
-  ret =  EAPChain(
-                  pargs["mlen"],
-                  pargs["E0"],
-                  pargs["K1"],
-                  pargs["K2"],
-                  pargs["kT"],
-                  pargs["Fz"],
-                  pargs["Fx"],
-                  ϕs,
-                  map(cos, ϕs),
-                  map(sin, ϕs),
-                  θs,
-                  map(cos, θs),
-                  map(sin, θs),
-                  zeros(3, pargs["num-monomers"]),
-                  zeros(pargs["num-monomers"]),
-                  zeros(3, pargs["num-monomers"]),
-                 );
-  for i=1:n(ret)
-    ret.μs[:, i] = μ(ret.E0, ret.K1, ret.K2, 
-                     ret.cϕs[i], ret.sϕs[i], ret.cθs[i], ret.sθs[i]);
-  end
-  ret.us[:] = map(i -> u(ret.E0, view(ret.μs, :, i)), 1:n(ret));
-  update_xs!(ret);
-  return ret;
-end
-
-# TODO: consider rewriting this to make better use of past calculations
-# this is probably by far the slowest calculation *shrug emoji*
-function U_interaction(chain::EAPChain)
-  U = 0.0;
-  for i=1:n(chain), j=i+1:n(chain) # once debugged, use inbounds
-    r = chain.xs[:, i] - chain.xs[:, j];
-    r2 = dot(r, r);
-    rmag = sqrt(r2);
-    r̂ = r / rmag;
-    r3 = r2*rmag;
-    μi = view(chain.μs, :, i);
-    μj = view(chain.μs, :, i);
-    U += (dot(μi, μj) - 3*dot(μi, r̂)*dot(μj, r̂)) / (4*π);
-  end
-  return U;
-end
-
-function move!(chain::EAPChain, idx::Int, dϕ::Real, dθ::Real)
-  ϕprev = chain.ϕs[idx];
-  chain.ϕs[idx] += dϕ;
-  if chain.ϕs[idx] < 0
-    chain.ϕs[idx] += 2*π;
-  elseif chain.ϕs[idx] >= 2*π
-    chain.ϕs[idx] -= 2*π;
-  end
-  chain.cϕs[idx] = cos(chain.ϕs[idx]);
-  chain.sϕs[idx] = sin(chain.ϕs[idx]);
-
-  θprev = chain.θs[idx]; 
-  chain.θs[idx] += dθ;
-  if chain.θs[idx] < 0
-    chain.θs[idx] = 0;
-  elseif chain.θs[idx] >= π
-    chain.θs[idx] = π;
-  end
-  chain.cθs[idx] = cos(chain.θs[idx]);
-  chain.sθs[idx] = sin(chain.θs[idx]);
-
-  chain.μs[:, idx] = μ(chain.E0, chain.K1, chain.K2,
-                       chain.cϕs[idx], chain.sϕs[idx], 
-                       chain.cθs[idx], chain.sθs[idx]);
-  chain.us[idx] = u(chain.E0, view(chain.μs, :, idx));
-  update_xs!(chain, idx);
-  return (chain.ϕs[idx] - ϕprev, chain.θs[idx] - θprev);
-end
-
-@inline end_to_end(chain::EAPChain) = (chain.xs[:, end] + 
-                                       chain.b/2*n̂j(chain, n(chain)));
-
-@inline function U(chain::EAPChain)
-  return (sum(chain.us) + U_interaction(chain)
-          - dot(end_to_end(chain), [chain.Fx; 0.0; chain.Fz]));
-end
-
-@inline chain_μ(chain::EAPChain) = sum(map(i -> chain.μs[:, i], 1:n(chain)));
+include("eap_chain.jl");
 
 s = ArgParseSettings();
 @add_arg_table! s begin
@@ -179,15 +42,15 @@ s = ArgParseSettings();
   "--num-steps", "-N"
     help = "number of steps"
     arg_type = Int
-    default = convert(Int, 1e6)
+    default = convert(Int, 1e5)
   "--phi-step", "-p"
     help = "maximum ϕ step length"
     arg_type = Float64;
-    default = π / 16;
+    default = 3*π / 8;
   "--theta-step", "-q"
     help = "maximum θ step length"
     arg_type = Float64;
-    default = π / 32;
+    default = 3*π / 16;
   "--update-freq", "-U"
     help = "update frequency (seconds)"
     arg_type = Float64;
@@ -195,7 +58,18 @@ s = ArgParseSettings();
   "--verbose", "-v"
     help = "verbosity level: 0-nothing, 1-errors, 2-warnings, 3-info"
     arg_type = Int
-    default = 2
+    default = 3
+  "--callbacks", "-C"
+    help = "file with callback functions"
+    arg_type = String
+  "--prefix", "-P"
+    help = "prefix for output files"
+    arg_type = String
+    default = "eap-mcmc"
+  "--postfix", "-Q"
+    help = "postfix for output files"
+    arg_type = String
+    default = ""
 end
 
 pargs = parse_args(s);
@@ -213,16 +87,28 @@ end
 metropolis_acc(kT::Real, dU::Real, ϵ::Real) = ( (dU <= 0) ? true : 
                                                ( ϵ <= exp(-dU / kT) ) );
 
-function mcmc(nsteps, pargs)
+callbacks = Any[];
+try
+  callbacks = if pargs["callbacks"] !== nothing
+                evalfile(pargs["callbacks"])
+              end
+catch e
+  @error "error occured loading $(pargs["callbacks"])";
+  rethrow();
+end
+
+function mcmc(nsteps::Int, pargs, callbacks)
   dϕ_dist = Uniform(-pargs["phi-step"], pargs["phi-step"]);
   dθ_dist = Uniform(-pargs["theta-step"], pargs["theta-step"]);
   chain = EAPChain(pargs);
-  Uprev = U(chain);
-  r = end_to_end(chain);
-  end_to_end_sum = r[:];
-  r2_sum = dot(r, r);
+  chain.U = U(chain);
+  end_to_end_sum = chain.r[:];
+  r2_sum = dot(chain.r, chain.r);
   chain_μ_sum = chain_μ(chain);
-  Usum = Uprev;
+  Usum = chain.U;
+  for callback in callbacks
+    callback(chain, 0, true, false, pargs);
+  end
 
   start = time();
   last_update = start;
@@ -233,11 +119,10 @@ function mcmc(nsteps, pargs)
     idx = rand(1:n(chain));
     ds = move!(chain, idx, dϕ, dθ);
     Ucurr = U(chain);
-    if metropolis_acc(chain.kT, Ucurr - Uprev, rand())
-      Uprev = Ucurr;
+    if metropolis_acc(chain.kT, Ucurr - chain.U, rand())
+      chain.U = Ucurr;
       num_accepted += 1;
     else # reverse move
-      Ucurr = Uprev;
       move!(chain, idx, -ds[1], -ds[2]);
     end
 
@@ -247,20 +132,28 @@ function mcmc(nsteps, pargs)
       last_update = time();
     end
 
-    r = end_to_end(chain);
-    end_to_end_sum += r[:];
-    r2_sum += dot(r, r);
+    for callback in callbacks
+      callback(chain, step, false, false, pargs);
+    end
+    end_to_end_sum += chain.r[:];
+    r2_sum += dot(chain.r, chain.r);
     chain_μ_sum += chain_μ(chain);
-    Usum += Ucurr;
+    Usum += chain.U;
+  
   end
 
   @info "total time elapsed: $(time() - start)";
   @info "acceptance rate: $(num_accepted / pargs["num-steps"])";
+
+  for callback in callbacks
+    callback(chain, pargs["num-steps"], false, true, pargs);
+  end
+
   return (end_to_end_sum, r2_sum, chain_μ_sum, Usum);
 
 end
 
-@show data = mcmc(pargs["num-steps"], pargs);
+@show data = mcmc(pargs["num-steps"], pargs, callbacks);
 
 println("<r>    =   $(data[1] / pargs["num-steps"])");
 println("<r/nb> =   $(data[1] / 
