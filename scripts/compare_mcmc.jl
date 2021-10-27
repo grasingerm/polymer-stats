1a100befff19f21b3b3dadfa6a726706066e5860
Matthew Grasinger <grasingerm@gmail.com>
2021-10-27 14:56:25 -0400

    working on script for post processing

--- scripts/compare_mcmc.jl
+++ scripts/compare_mcmc.jl
@@ -1,40 +1,59 @@
 using Glob;
 using DelimitedFiles;
-using Quadmath;
-using DecFP;
+using GLM;
+using DataFrames;
+using Logging;
 
-if length(ARGS) != 3
-  println("usage: julia statistical_analysis_mcmc.jl <outfile> <indir> <pattern>");
+if length(ARGS) != 2
+  println("usage: julia statistical_analysis_mcmc.jl <indir> <subpattern>");
   exit(1);
 end
 
-outfile = open(ARGS[1], "w");
-indir = ARGS[2];
-pattern = Glob.GlobMatch(ARGS[3]);
+global_logger(ConsoleLogger(stderr, Logging.Info));
 
-data_series = Dict();
-for infile in readdir(pattern, indir)
-  for line in readlines(infile)
-    var, val = map(strip, split(line, "="));
-    val = eval(Meta.parse(val));
-    if haskey(data_series, var)
-      push!(data_series[var], val);
-    else
-      data_series[var] = [val];
+indir = ARGS[1];
+subpattern = ARGS[2];
+pattern = Glob.GlobMatch(subpattern * ".out");
+
+chunks = map(piece -> split(piece, "-"), split(subpattern, "_"));
+param_map = Dict();
+for chunk in chunks
+  if chunk[1] == "run"; continue; end
+  param_map[chunk[1]] = Meta.parse(chunk[end]) / 1000.0;
+end
+@show param_map;
+
+data_serieses = Dict();
+for subdir in ["standard"; "clustering"; "umbrella"]
+  data_series = Dict();
+  for infile in readdir(pattern, joinpath(indir, subdir))
+    for line in readlines(infile)
+      var, val = map(strip, split(line, "="));
+      val = eval(Meta.parse(val));
+      if haskey(data_series, var)
+        push!(data_series[var], val);
+      else
+        data_series[var] = [val];
+      end
     end
   end
+  data_serieses[subdir] = data_series;
 end
 
-avgs = Dict();
-vars = Dict();
-std_norms = Dict();
-for (k, v) in data_series
-  avgs[k] = sum(v) / length(v);
-  vars[k] = sum(map(x -> (x - avgs[k]).^2, v)) / length(v);
-  std_norms[k] = map(sqrt, vars[k]) ./ avgs[k];
+Ustar = (param_map["E0"])^2 * abs(param_map["K2"] - param_map["K1"]);
+rstar = param_map["b"];
+μstar = (param_map["E0"])*(2*param_map["K2"] + param_map["K1"]);
+for (name, data_series) in data_serieses
+  @show name;
+  avgs = Dict();
+  vars = Dict();
+  std_norms = Dict();
+  for (k, v) in data_series
+    avgs[k] = sum(v) / length(v);
+    vars[k] = sum(map(x -> (x - avgs[k]).^2, v)) / length(v);
+    std_norms[k] = map(sqrt, vars[k]) ./ avgs[k];
+  end
+  for k in keys(avgs)
+    @info "$k: μ = $(avgs[k]); σ^2 = $(vars[k]); σ / μ = $(std_norms[k])";
+  end
 end
-for k in keys(avgs)
-  @info "$k: μ = $(avgs[k]); σ^2 = $(vars[k]); σ / μ = $(std_norms[k])";
-end
-
-close(outfile);
