9d56e9cc6a2471e6256c013d8370d47785283bc0
Matthew Grasinger <grasingerm@gmail.com>
2022-08-17 16:26:48 -0400

    added script for aggregation and added some more parameter studies

---
+++ run/phases_2022-08-17.jl
@@ -0,0 +1,50 @@
+println("Hello world!");
+
+@everywhere using Printf;
+
+@show workdir = if length(ARGS) > 0
+  ARGS[1];
+else
+  ".";
+end
+
+@everywhere fmt(x) = @sprintf("%07d", round(Int, 1e3*x));
+@everywhere fmt_int(x) = @sprintf("%03d", x);
+
+@everywhere function prefix(case)
+  "E0-$(fmt(case[:E0]))_K1-$(fmt(case[:K1]))_K2-$(fmt(case[:K2]))_kT-$(fmt(case[:kT]))_Fz-$(fmt(case[:Fz]))_Fx-$(fmt(case[:Fx]))_n-$(fmt(case[:n]))_b-$(fmt(case[:b]))_kappa-$(fmt(case[:kappa]))";
+end
+
+cases = Any[];
+κs = [0.0];
+Ks = zip([1.0], [0.0]);
+E0s = vcat(0.0, 0.005:0.005:1.0);
+ns = Int[100];
+bs = [1.0];
+kTs = [1.0];
+#Fs = zip([0.0; 1.0; 0.0], [0.0; 0.0; 1.0]);
+Fs = zip([0.0], [0.0]);
+for b in bs, κ in κs, n in ns, Kvec in Ks, E0 in E0s, kT in kTs, F in Fs
+  K1, K2 = Kvec;
+  push!(cases, Dict(:E0 => E0, :K1 => K1, :K2 => K2,
+                    :kT => kT, :Fz => F[2], :kappa => κ,
+                    :Fx => F[1], :n => n, :b => b));
+end
+
+@info "total number of cases to run: $(length(cases))";
+
+mkpath(workdir);
+
+pmap(case -> begin;
+
+  outfile = joinpath(workdir, "$(prefix(case)).out");
+  if !isfile(outfile)
+    println("Running case: $case.");
+    command = `julia -O 3 mcmc_clustering_eap_chain.jl --chain-type dielectric --energy-type interacting -b $(case[:b]) --bend-mod $(case[:kappa]) --E0 $(case[:E0]) --K1 $(case[:K1]) --K2 $(case[:K2]) --kT $(case[:kT]) --Fz $(case[:Fz]) --Fx $(case[:Fx]) -n $(case[:n]) --num-steps 2000000 --burn-in 200000 -v 2 --prefix $(joinpath(workdir, prefix(case)))`;
+    output = read(command, String);
+    write(outfile, output); 
+  else
+    println("Case: $case has already been run.");
+  end
+
+end, cases);
---
+++ scripts/aggregate_by.jl
@@ -0,0 +1,45 @@
+using Glob;
+
+if length(ARGS) < 4
+  println("usage: julia aggregate_by.jl <outdir> <indir> <param> <dielectric|polar> [<3D|2D>]");
+  exit(1);
+end
+
+dims = (length(ARGS) == 5) ? ARGS[5] : "3D";
+
+outdir = ARGS[1];
+indir = ARGS[2];
+param = ARGS[3];
+chain_type = ARGS[4];
+
+mkpath(outdir);
+
+datafiles = readdir(glob"*.out", indir);
+params_ran = [];
+for datafile in datafiles
+  datafile = basename(datafile);
+  fileparams = split(string(split(basename(datafile), ".")[1:end-1]...), "_");
+  @show filtered_params = filter(x -> !startswith(x, param), fileparams);
+  if filtered_params in params_ran
+    println("-- param set already run -- \n");;
+    continue;
+  else
+    push!(params_ran, filtered_params);
+  end
+  @show datafile, "$param-"
+  @show strspan = findnext("$param-", datafile, 1);
+  if strspan == nothing; continue; end
+  value_start_index = strspan[end]+1;
+  @show strspan2 = findnext("_", datafile, value_start_index);
+  value_end_index = (strspan2 != nothing) ? strspan2[1] : length(datafile)-3;
+  @show pattern = string(datafile[1:value_start_index-1], "*", 
+                         datafile[value_end_index:end])
+  @show value = datafile[value_start_index:value_end_index-1];
+  @show outfile = joinpath(outdir, join(filtered_params, "_")*".csv");
+  @show command = `julia scripts/aggregate_mcmc.jl $outfile $indir "$pattern" $chain_type $dims`;
+  @show process_output = readlines(command);
+end
+
+display(params_ran);
+println();
+@show length(params_ran);
