3f263e9f977011b06de20ec0b11b18122a2cf659
Matthew Grasinger <grasingerm@gmail.com>
2023-05-18 11:11:23 -0400

    typo in study for long chains

--- run/phases_2023-05-15b.jl
+++ run/phases_2023-05-15b.jl
@@ -17,10 +17,10 @@ end
 
 cases = Any[];
 κs = [0.0];
-Ks = zip([1.0; 0.0], [0.0; 1.0]);
+Ks = zip([1.0; 0.0], [0.0; 1.0], [2.0; 1.0], [1.0; 2.0]);
 E0s = vcat(0.05:0.05:1.0, 1.5:0.5:5.0, 10.0);
-ns = Int[400];
-bs = [0.5; 1.0];
+ns = Int[40; 50];
+bs = [0.1; 0.25; 0.5; 1.0];
 kTs = [0.1; 1.0];
 Fs = zip([0.0; 1.0; 0.0; 1.0/sqrt(2.0)], [0.0; 0.0; 1.0; 1.0/sqrt(2.0)]);
 for b in bs, κ in κs, n in ns, Kvec in Ks, E0 in E0s, kT in kTs, F in Fs
@@ -39,7 +39,7 @@ pmap(case -> begin;
   outfile = joinpath(workdir, "$(prefix(case)).out");
   if !isfile(outfile)
     println("Running case: $case.");
-    command = `julia -O 3 mcmc_clustering_eap_chain.jl --chain-type dielectric --energy-type cutoff -b $(case[:b]) --bend-mod $(case[:kappa]) --E0 $(case[:E0]) --K1 $(case[:K1]) --K2 $(case[:K2]) --kT $(case[:kT]) --Fz $(case[:Fz]) --Fx $(case[:Fx]) -n $(case[:n]) --num-steps 2000000 --burn-in 200000 -v 2 --prefix $(joinpath(workdir, prefix(case)))`;
+    command = `julia -O 3 mcmc_clustering_eap_chain.jl --chain-type dielectric --energy-type interacting -b $(case[:b]) --bend-mod $(case[:kappa]) --E0 $(case[:E0]) --K1 $(case[:K1]) --K2 $(case[:K2]) --kT $(case[:kT]) --Fz $(case[:Fz]) --Fx $(case[:Fx]) -n $(case[:n]) --num-steps 2000000 --burn-in 200000 -v 2 --prefix $(joinpath(workdir, prefix(case)))`;
     output = read(command, String);
     write(outfile, output); 
   else
