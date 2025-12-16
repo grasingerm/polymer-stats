47825cb573528d8456a12c31b92abaecca19b0a0
Matthew Grasinger <grasingerm@gmail.com>
2025-12-16 10:42:29 -0500

    updated aggregation script to consider Fx and Fz simultaneously

--- scripts/aggregate_by.jl
+++ scripts/aggregate_by.jl
@@ -28,7 +28,11 @@ for datafile in datafiles
   datafile = basename(datafile);
   fileparams = split(string(split(basename(datafile), ".")[1:end-1]...), "_");
   if runflag; pop!(fileparams); end
-  @show filtered_params = filter(x -> !startswith(x, param), fileparams);
+  @show filtered_params = if FxFzFlag
+      filter(x -> !startswith(x, "Fz") && !startswith(x, "Fx"), fileparams)
+  else
+      filter(x -> !startswith(x, param), fileparams)
+  end
   if filtered_params in params_ran
     println("-- param set already run -- \n");;
     continue;
@@ -41,9 +45,12 @@ for datafile in datafiles
   value_start_index = strspan[end]+1;
   @show strspan2 = findnext("_", datafile, value_start_index);
   value_end_index = (strspan2 != nothing) ? strspan2[1] : length(datafile)-3;
-  pattern = string(datafile[1:value_start_index-1], "*", datafile[value_end_index:end])
-  if FxFzFlag
-      pattern *= pattern
+  pattern = if FxFzFlag
+      start_index = findnext("Fz-", datafile, 1).start
+      end_index = findnext("_", datafile, findnext("Fx-", datafile, 1).stop).stop-1
+      string(datafile[1:start_index-1] * "Fz-*_Fx-*" * datafile[end_index+1:end])
+  else
+      string(datafile[1:value_start_index-1], "*", datafile[value_end_index:end])
   end
   @show pattern = (runflag) ? pattern[1:end-5] * "*" * pattern[end-3:end] : pattern
   @show value = datafile[value_start_index:value_end_index-1];
